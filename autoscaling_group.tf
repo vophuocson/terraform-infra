cc74b625c653a7ac5a8a15383e1ccaa6b3bba7fb
son-vp1998 <son-vp@genkisystem.com>
2025-09-13 19:27:40 +0700

    change launch config to launch template

--- autoscaling_group.tf
+++ autoscaling_group.tf
@@ -1,7 +1,10 @@
 resource "aws_autoscaling_group" "example" {
   min_size = 2
   max_size = 10
-  launch_configuration = aws_launch_configuration.exemple.name
+  launch_template {
+    id = aws_launch_template.example.id
+    version = "$Latest"
+  }
   vpc_zone_identifier = data.aws_subnets.default.ids
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
--- lunch_configtion.tf
+++ lunch_configtion.tf
@@ -1,12 +1,18 @@
-resource "aws_launch_configuration" "exemple" {
-  image_id = "ami-097f32b3a493c5a96"
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-lt-"
+  image_id      = "ami-097f32b3a493c5a96"
   instance_type = "t3.micro"
-  security_groups = [aws_security_group.instance.id]
-  user_data = <<-EOF
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = base64encode(<<-EOF
     #!/bin/bash
     echo "Hello, World" > index.xhtml
     nohup busybox httpd -f -p ${var.server_port} &
-    EOF
+  EOF
+  )
+  
   lifecycle {
     create_before_destroy = true
   }

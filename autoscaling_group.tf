6ae40ded47b71a2c3f06daa1fde2e5bcfcf911d4
son-vp1998 <son-vp@genkisystem.com>
2025-09-13 08:51:30 +0700

    add auto scalling

---
+++ autoscaling_group.tf
@@ -0,0 +1,13 @@
+resource "aws_autoscaling_group" "example" {
+  min_size = 2
+  max_size = 10
+  launch_configuration = aws_launch_configuration.exemple.name
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}

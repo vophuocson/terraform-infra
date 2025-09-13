d63f7f2c8dd87c71da6b58c22ee5dcb0e9908839
son-vp1998 <son-vp@genkisystem.com>
2025-09-13 08:47:12 +0700

    add security group for load balancer

--- security.tf
+++ security.tf
@@ -7,3 +7,20 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

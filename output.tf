493366734f1dd0ae592ff8c51cc741edf2065421
son-vp1998 <son-vp@genkisystem.com>
2025-09-13 19:28:45 +0700

    remove ip, add dns name

--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
-output "public_ip" {
-  value = aws_instance.instance_example.public_ip
-  description = "The public IP address of the web server"
-  depends_on = [ aws_security_group.instance ]
+output "alb_dns_name" {
+  value = aws_lb.example.dns_name
+  description = "The domain name of the load balancer"
 }

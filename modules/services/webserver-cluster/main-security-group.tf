983b6f82d0635f91df63d680724c187aaaf00d80
son-vp1998 <son-vp@genkisystem.com>
2025-09-18 20:31:30 +0700

    separate inbound and oubound out of security group

--- modules/services/webserver-cluster/main-security-group.tf
+++ modules/services/webserver-cluster/main-security-group.tf
@@ -11,17 +11,22 @@ resource "aws_security_group" "instance" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
-  ingress {
-    from_port = local.http_port
-    to_port = local.http_port
-    protocol = local.tcp_protocol
-    cidr_blocks = local.all_ips
-  }
-  
-  egress {
-    from_port = local.any_port
-    to_port = local.any_port
-    protocol = local.any_protocol
-    cidr_blocks = local.all_ips
-  }
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }

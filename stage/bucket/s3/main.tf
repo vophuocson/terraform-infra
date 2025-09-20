35de24bdf20370d1e5f5773376ab3f44f061debf
son-vp1998 <son-vp@genkisystem.com>
2025-09-20 16:38:28 +0700

    remove comment code

--- stage/bucket/s3/main.tf
+++ stage/bucket/s3/main.tf
@@ -1,14 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     key = "global/s3/terraform.tfstate"
-#     bucket = "stage-son-vp-terraform-up-and-running-state"
-#     encrypt = true
-#     use_lockfile   = true 
-#     region = "ap-southeast-1"
-#   }
-# }
-
-
 module "s3" {
   source = "../../../modules/bucket/s3"
   bucket_name = "stage-son-vp-terraform-up-and-running-state"

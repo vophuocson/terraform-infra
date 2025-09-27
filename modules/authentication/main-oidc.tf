0d6921d329e5e47e366799ec26f213ee65b24668
son-vp1998 <son-vp@genkisystem.com>
2025-09-27 19:31:18 +0700

    add role

--- modules/authentication/main-oidc.tf
+++ modules/authentication/main-oidc.tf
@@ -7,3 +7,8 @@ resource "aws_iam_openid_connect_provider" "github_action" {
   client_id_list = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
+
+resource "aws_iam_role" "auth_role" {
+  name = "AuthenticationRole"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}

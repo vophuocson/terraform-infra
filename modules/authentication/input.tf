cd1ef203cdeb491a750d4f3b527d7e5e39482222
son-vp1998 <son-vp@genkisystem.com>
2025-09-27 10:09:47 +0700

    #3919 add authi creadential aws by github OIDC

---
+++ modules/authentication/input.tf
@@ -0,0 +1,30 @@
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com"
+}
+
+variable "allowed_repos_branches" {
+  description = "GitHub repos/branches allowed to assume the IAM role."
+  type = list(object({
+    org = string
+    repo = string
+    branch = string
+  }))
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect = "Allow"
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+      type = "Federated"
+    }
+    condition {
+      test = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [ 
+        for a in var.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
+      ]
+    }
+  }
+}
---
+++ modules/authentication/main-oidc.tf
@@ -0,0 +1,5 @@
+resource "aws_iam_openid_connect_provider" "github_action" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
+}

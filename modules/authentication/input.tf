e24e216287b831f25c209669ddc87adf4a6d3803
son-vp1998 <son-vp@genkisystem.com>
2025-09-27 20:31:57 +0700

    add policy to role

--- modules/authentication/input.tf
+++ modules/authentication/input.tf
@@ -10,21 +10,3 @@ variable "allowed_repos_branches" {
     branch = string
   }))
 }
-
-data "aws_iam_policy_document" "assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRoleWithWebIdentity"]
-    effect = "Allow"
-    principals {
-      identifiers = [aws_iam_openid_connect_provider.github_action.arn]
-      type = "Federated"
-    }
-    condition {
-      test = "StringEquals"
-      variable = "token.actions.githubusercontent.com:sub"
-      values = [ 
-        for a in var.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
-      ]
-    }
-  }
-}
--- modules/authentication/main-oidc.tf
+++ modules/authentication/main-oidc.tf
@@ -3,12 +3,36 @@ provider "aws" {
 }
 
 resource "aws_iam_openid_connect_provider" "github_action" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = [data.tls_certificate.github.certificates[0].sha1_fingerprint]
 }
 
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.github_action.arn]
+      type        = "Federated"
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values = [
+        for a in var.allowed_repos_branches : "repo:${a["org"]}/${a["repo"]}:ref:refs/heads/${a["branch"]}"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "auth_role_admin" {
+  role = aws_iam_role.auth_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+
 resource "aws_iam_role" "auth_role" {
-  name = "AuthenticationRole"
+  name               = "AuthenticationRole"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
